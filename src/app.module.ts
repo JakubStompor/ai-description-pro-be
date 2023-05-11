b94d445cac582e3f4060352fa1c95763f6c112d2
Jakub Stompor <jakub.stompor@external.telekom.de>
2023-05-11 09:45:08 +0200

    CB-4 cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,28 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { configValidationSchema } from './config/config.schema';
 import { ProductsModule } from './products/products.module';
 import { AiModule } from './ai/ai.module';
-import { AuthModule } from './auth/auth.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'products-management',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
     ConfigModule.forRoot({
       envFilePath: [`.env.stage.${process.env.STAGE}`],
       validationSchema: configValidationSchema,
     }),
     ProductsModule,
     AiModule,
-    AuthModule,
   ],
 })
 export class AppModule {}
